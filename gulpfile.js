38ba7975a7dbd57d76e471fcbf5a7cb54dffb5bb
sangrampp <sangrampp@gmail.com>
2015-10-28 19:52:20 +0530

    Setup gulp with all sass files

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+//include gulp
+var gulp = require('gulp')
+
+//include plugins
+var sass = require('gulp-sass')
+
+//compile sass
+gulp.task('sass', function() {
+    return gulp.src('css/src/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+//watch files for changes
+gulp.task('watch', function() {
+    gulp.watch('css/src/*.scss', ['sass']);
+});
+
+//default task
+gulp.task('default', ['sass']);
